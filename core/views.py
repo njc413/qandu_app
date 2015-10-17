d0472a2e4715247238cd3f69502be57eb8da0804
njc413 <njc413@aim.com>
2015-10-17 02:06:59 +0000

    add answer voting

--- core/views.py
+++ core/views.py
@@ -107,13 +107,26 @@ class VoteFormView(FormView):
     form_class = VoteForm
 
     def form_valid(self, form):
-      user = self.request.user
-      question = Question.objects.get(pk=form.data["question"])
-      prev_votes = Vote.objects.filter(user=user, question=question)
-      has_voted = (prev_votes.count()>0)
-      if not has_voted:
-         Vote.objects.create(user=user, question=question)
-      else:
-         prev_votes[0].delete()
-      return redirect('question_list')
+        user = self.request.user
+        question = Question.objects.get(pk=form.data["question"])
+        try:
+            answer = Answer.objects.get(pk=form.data[ "answer" ])
+            prev_votes = Vote.objects.filter(user=user, answer=answer)
+            has_voted = (prev_votes.count()>0)
+            if not has_voted:
+                Vote.objects.create(user=user, question=question)
+            else:
+                prev_votes[0].delete()
+            return redirect(reserve('question_detail', args=[form.data["question"]]))
+        except:
+            prev_votes = Vote.objects.filter(user=user, question=question)
+            has_voted = (prev_votes.count()>0)
+            if not has_voted:
+                Vote.objects.create(user=user, question=question)
+            else:
+                prev_votes[0].delete()
+        return redirect('question_list')
+
+
+
 
